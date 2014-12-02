7ce2fc2e297f4b5bb2721f16480ea4a64ef980f8
Eduardo Costa <m4c0@users.noreply.github.com>
2014-12-02 18:02:12 -0200

    Criando projeto

---
+++ Ogle/Ogle.h
@@ -0,0 +1,19 @@
+//
+//  Ogle.h
+//  Ogle
+//
+//  Created by Eduardo Mauricio da Costa on 02/12/14.
+//  Copyright (c) 2014 Eduardo Mauricio da Costa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Ogle.
+FOUNDATION_EXPORT double OgleVersionNumber;
+
+//! Project version string for Ogle.
+FOUNDATION_EXPORT const unsigned char OgleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Ogle/PublicHeader.h>
+
+
